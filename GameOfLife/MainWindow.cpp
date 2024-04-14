61696ed5f58b6b6f9ebd359e64539c623e05ce9e
Gabriel Pierre <gabriel.pierre47@yahoo.com>
2024-04-14 13:15:45 -0400

    -Completed 2.7 and 2.8 added status bar and tool bar -added private variables for generation and livingcells -have a status bar variable that is intialized -made a setbartext method for toolbar..adeed tool bar variable toolbar and initiliazed it - added play,next,trash ,and pause includes at top of header file - created bitmap object for play,, next, trash icons -added objects to tool bar with set parameters. for id i used a static const int variable for ids -made new even handlers for each icon -add entry to event table for each icon -called - realize method and the did check if statemtn for setsize

--- GameOfLife/MainWindow.cpp
+++ GameOfLife/MainWindow.cpp
@@ -1,28 +1,77 @@
 #include "MainWindow.h"
 #include "DrawingPanel.h"
+#include "play.xpm"
+#include "pause.xpm"
+#include "next.xpm"
+#include "trash.xpm"
+
 wxBEGIN_EVENT_TABLE(MainWindow, wxFrame)
 	EVT_SIZE(MainWindow::OnSizeChange)
+	EVT_MENU(MainWindow::PLAY_ID, MainWindow::OnPlay)
+	EVT_MENU(MainWindow::PAUSE_ID, MainWindow::OnPause)
+	EVT_MENU(MainWindow::NEXT_ID, MainWindow::OnNext)
+	EVT_MENU(MainWindow::TRASH_ID, MainWindow::OnTrash)
 wxEND_EVENT_TABLE()
 
 
 MainWindow::MainWindow() :wxFrame(nullptr, wxID_ANY, "Game of Life", wxPoint(0, 0), wxSize(500, 300))
 {
+	//tool bar
+	toolBar = CreateToolBar();
+	wxBitmap playIcon(play_xpm);
+	wxBitmap pauseIcon(pause_xpm);
+	wxBitmap nextIcon(next_xpm);
+	wxBitmap trashIcon(trash_xpm);
+	toolBar->AddTool(PLAY_ID, "Play", playIcon);
+	toolBar->AddTool(PAUSE_ID, "Pause", pauseIcon);
+	toolBar->AddTool(NEXT_ID, "Next", nextIcon);
+	toolBar->AddTool(TRASH_ID, "Trash", trashIcon);
+	toolBar->Realize();
+	//status bar
+	statusBar = CreateStatusBar();
 	DrawingPanel* panel = new DrawingPanel(this, gridstates);
 	boxSizer = new wxBoxSizer(wxVERTICAL);
 	boxSizer->Add(panel, 1, wxEXPAND | wxALL);
 	this->panel = panel;
 	this->SetSizer(boxSizer);
 
+
+	
 	//this->Bind(wxEVT_SIZE, &MainWindow::OnSizeChange, this);
 
 	this->GridInitialize();
+
+	this->Layout();
 }
 
 void MainWindow::OnSizeChange(wxSizeEvent& event)
 {
 	wxSize mySize = this->GetClientSize();
-	panel->SetSize(mySize);
+	if(panel != nullptr)
+	{ 
+		panel->SetSize(mySize);
+	}
 	event.Skip();
+}
+
+void MainWindow::OnPlay(wxCommandEvent& event)
+{
+
+}
+
+void MainWindow::OnPause(wxCommandEvent& event)
+{
+
+}
+
+void MainWindow::OnNext(wxCommandEvent& event)
+{
+
+}
+
+void MainWindow::OnTrash(wxCommandEvent& event)
+{
+
 }
 
 //grid initialize
@@ -40,6 +89,11 @@ void MainWindow::GridInitialize()
 	}
 
 	panel->SetGridSize(GridSize);
+}
+
+void MainWindow::SetStatusBarText(const wxString& text)
+{
+	statusBar->SetStatusText(text);
 }
 
 MainWindow::~MainWindow()
--- GameOfLife/MainWindow.h
+++ GameOfLife/MainWindow.h
@@ -7,12 +7,25 @@ class MainWindow : public wxFrame
 {
 	DrawingPanel* panel = nullptr; // all variables in header files need to be intialize 
 	wxBoxSizer* boxSizer = nullptr;
-public:
+	int generation;
+	int livingCells;
+	wxStatusBar* statusBar;
+	wxToolBar* toolBar;
 	
+public:
+	static const int PLAY_ID = 10813;
+	static const int PAUSE_ID = 10654;
+	static const int NEXT_ID = 10433;
+	static const int TRASH_ID = 10999;
 	MainWindow();
 	~MainWindow();
 	void OnSizeChange(wxSizeEvent&);
 	void GridInitialize();
+	void SetStatusBarText(const wxString &text);
+	void OnPlay(wxCommandEvent&);
+	void OnPause(wxCommandEvent&);
+	void OnNext(wxCommandEvent&);
+	void OnTrash(wxCommandEvent&);
 	std::vector<std::vector<bool>> gridstates;
 	int GridSize = 15;
 	wxDECLARE_EVENT_TABLE();
