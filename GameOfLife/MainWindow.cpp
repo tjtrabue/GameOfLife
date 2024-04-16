a2cc3f80e950371cd2aa3b75df170e944c8c8e0e
Gabriel Pierre <gabriel.pierre47@yahoo.com>
2024-04-16 16:33:31 -0400

    MILESTONE SUBMISSION

--- GameOfLife/MainWindow.cpp
+++ GameOfLife/MainWindow.cpp
@@ -43,7 +43,6 @@ MainWindow::MainWindow() :wxFrame(nullptr, wxID_ANY, "Game of Life", wxPoint(0, 
 	//this->Bind(wxEVT_SIZE, &MainWindow::OnSizeChange, this);
 
 	this->GridInitialize();
-	//
 	this->Layout();
 }
 
